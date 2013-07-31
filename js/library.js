c7ee6a5279d080d266b66a1bc0f38538b742f160
Eugene Manuilov <eugene@manuilov.org>
2013-07-31 22:23:05 +0300

    Fade in effect was added for charts appearance in the library.

--- js/library.js
+++ js/library.js
@@ -30,16 +30,20 @@
 	var resizeTimeout;
 
 	$.fn.adjust = function() {
-		var width = $('#visualizer-library').width(),
-			margin = width * 0.02;
+		return $(this).each(function() {
+			var width = $('#visualizer-library').width(),
+				margin = width * 0.02;
 
-		width *= 0.305;
-		$(this).width(width - 14).height(width * 0.75).parent().css('margin-right', margin + 'px').css('margin-bottom', margin + 'px');
+			width *= 0.305;
+			$(this).width(width - 14).height(width * 0.75).parent().css('margin-right', margin + 'px').css('margin-bottom', margin + 'px');
+		});
 	}
 
 	$('.visualizer-chart-canvas').adjust();
 
 	$(document).ready(function() {
+		$('.visualizer-chart').fadeIn(500);
+
 		$('.visualizer-chart-shortcode').click(function(e) {
 			var range, selection;
 
