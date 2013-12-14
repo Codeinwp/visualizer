c140a2ded56b9ebd9877421889c1bbe1a89c964c
Eugene Manuilov <eugene@manuilov.org>
2013-12-14 22:54:49 +0200

    Implemented focus target and data opacity for columnar chars

--- classes/Visualizer/Render/Sidebar/Columnar.php
+++ classes/Visualizer/Render/Sidebar/Columnar.php
@@ -44,14 +44,38 @@ abstract class Visualizer_Render_Sidebar_Columnar extends Visualizer_Render_Side
 		self::_renderGroupStart( esc_html__( 'Bars Settings', Visualizer_Plugin::NAME ) );
 			self::_renderSectionStart();
 				self::_renderSelectItem(
+					esc_html__( 'Focus Target', Visualizer_Plugin::NAME ),
+					'focusTarget',
+					$this->focusTarget,
+					array(
+						''         => '',
+						'datum'    => esc_html__( 'Focus on a single data point.', Visualizer_Plugin::NAME ),
+						'category' => esc_html__( 'Focus on a grouping of all data points along the major axis.', Visualizer_Plugin::NAME ),
+					),
+					esc_html__( 'The type of the entity that receives focus on mouse hover. Also affects which entity is selected by mouse click.', Visualizer_Plugin::NAME )
+				);
+
+				echo '<div class="section-delimiter"></div>';
+
+				self::_renderSelectItem(
 					esc_html__( 'Is Stacked', Visualizer_Plugin::NAME ),
 					'isStacked',
 					$this->isStacked,
 					$this->_yesno,
 					esc_html__( 'If set to yes, series elements are stacked.', Visualizer_Plugin::NAME )
 				);
+
+				echo '<div class="section-delimiter"></div>';
+
+				self::_renderTextItem(
+					esc_html__( 'Bars Opacity', Visualizer_Plugin::NAME ),
+					'dataOpacity',
+					$this->dataOpacity,
+					esc_html__( 'Bars transparency, with 1.0 being completely opaque and 0.0 fully transparent.', Visualizer_Plugin::NAME ),
+					'1.0'
+				);
 			self::_renderSectionEnd();
 		self::_renderGroupEnd();
 	}
-	
+
 }
