9b4cadd420a63489cf74ce40e505888308d22a24
selu91 <cristea.marius.alexandru@gmail.com>
2017-03-13 12:59:49 +0200

    updated travis

---
+++ tests/Import_Test.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Test class for the importing features.
+ *
+ * @package     Visualizer
+ * @subpackage  Tests
+ * @copyright   Copyright (c) 2017, Marius Cristea
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       2.0.0
+ *
+ */
+class Import_Test extends WP_UnitTestCase  {
+
+}
