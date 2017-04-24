b8532cac49e50126dc5c61a8f79b5626c71ea54f
selu91 <cristea.marius.alexandru@gmail.com>
2017-04-24 18:03:07 +0300

    Fixed logger flag.

--- classes/Visualizer/Module/Admin.php
+++ classes/Visualizer/Module/Admin.php
@@ -59,7 +59,6 @@ class Visualizer_Module_Admin extends Visualizer_Module {
 		$this->_addFilter( 'media_view_strings', 'setupMediaViewStrings' );
 		$this->_addFilter( 'plugin_action_links', 'getPluginActionLinks', 10, 2 );
 		$this->_addFilter( 'plugin_row_meta', 'getPluginMetaLinks', 10, 2 );
-		$this->_addFilter( 'visualizer_logger_flag', 'get_logger_flag', 10, 1 );
 		$this->_addAjaxAction( Visualizer_Plugin::ACTION_TRACK, 'visualizer_enable_track' );
 	}
 
@@ -75,17 +74,6 @@ class Visualizer_Module_Admin extends Visualizer_Module {
 			update_option( 'visualizer_logger_flag', 'no' );
 		}
 		wp_send_json_success( array( 'status' => $status ) );
-	}
-
-	/**
-	 * Either the tracking is active or not.
-	 *
-	 * @return bool The flag status.
-	 */
-	public function get_logger_flag() {
-		$flag = get_option( 'visualizer_logger_flag', 'no' );
-
-		return ( $flag === 'yes' );
 	}
 
 	/**
