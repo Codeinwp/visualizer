1a999dd1ca88c8742c6e75a29ff5613e4b20c5fe
selu91 <cristea.marius.alexandru@gmail.com>
2017-11-16 12:55:26 +0200

    Added faq sync with helpscout.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,5 +6,10 @@ module.exports = function (grunt) {
 
 	var loader = require( 'load-project-config' ),
 		config = require( 'grunt-plugin-fleet' );
+	config = config();
+	// jshint ignore: start
+	config.taskMap['faq_builder'] = 'grunt-helpscout-faq';
+	// jshint ignore: end
+	loader(grunt, config).init();
 	loader( grunt, config ).init();
 };
