ae91dc3df00524e10de2e9975b6d94ae85b2eddf
contactashish13 <contactashish13@gmail.com>
2018-04-23 13:57:56 +0530

    undo changes that were not required

--- js/render.js
+++ js/render.js
@@ -157,9 +157,7 @@
         for (i = 0; i < data.length; i++) {
 			row = [];
 			for (j = 0; j < series.length; j++) {
-				if (series[j].type === 'date' || series[j].type === 'datetime') {
-                    data[i][j]=new Date("2013-01-30");
-                    
+				if (series[j].type === 'date' || series[j].type === 'datetime') {                  
 					date = new Date(data[i][j]);
 					data[i][j] = null;
 					if (Object.prototype.toString.call(date) === "[object Date]") {
@@ -167,8 +165,6 @@
 							data[i][j] = date;
 						}
 					}
-                    
-                    console.log(data[i][j]);
 				}
 				row.push(data[i][j]);
 			}
@@ -204,7 +200,6 @@
         }
 
         v.override(settings);
-console.log(table);
         render.draw(table, settings);
 	};
 
