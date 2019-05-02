39f3e55da68c8ae9a8fe4cae7aee2fd8396a7c31
Ashish Ravi <contactashish13@gmail.com>
2019-05-02 17:14:32 +0530

    hide alternate simple editor checkbox when a simple editor is active

--- classes/Visualizer/Render/Page/Data.php
+++ classes/Visualizer/Render/Page/Data.php
@@ -390,7 +390,7 @@ class Visualizer_Render_Page_Data extends Visualizer_Render_Page {
 								<div>
 									<p class="viz-group-description"><?php echo sprintf( __( 'You can manually edit the chart data using the %s editor.', 'visualizer' ), VISUALIZER_PRO ? 'spreadsheet like' : 'simple' ); ?></p>
 									<?php if ( ! VISUALIZER_PRO ) { ?>
-										<p class="viz-group-description"><input type="checkbox" id="simple-editor-type" value="textarea"><label for="simple-editor-type"><?php _e( 'Use text area editor instead', 'visualizer' ); ?></label></p>
+										<p class="viz-group-description simple-editor-type"><input type="checkbox" id="simple-editor-type" value="textarea"><label for="simple-editor-type"><?php _e( 'Use text area editor instead', 'visualizer' ); ?></label></p>
 									<?php } ?>
 									<input type="button" id="editor-chart-button" class="button button-primary "
 										   value="<?php _e( 'View Editor', 'visualizer' ); ?>" data-current="chart"
