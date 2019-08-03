426bf8bbfa878d586fc35a4d2b6fe3a5e0246b46
Ashish Ravi <contactashish13@gmail.com>
2019-08-03 10:47:22 +0530

    Add links to survey

---
+++ templates/improve.php
@@ -0,0 +1,11 @@
+	<div class="pro-feature">
+		<div class="pro-feature-inner">
+			<div class="pro-feature-features">
+				<h2>Some heading</h2>
+				<p>Some lovely text.</p>
+				<p><a href="<?php echo VISUALIZER_SURVEY; ?>" target="_blank">the survey</a></p>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<div class="clear"></div>
