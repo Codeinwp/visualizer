7224e4f951808249c061b31888f5deb15ed0137a
Hardeep Asrani <hardeepasrani@gmail.com>
2019-10-08 23:35:28 +0530

    Fix visualizer rest query
    This filters the rest query to make sure no ChartJS charts are being
    shown file others appear just fine, even when the meta value
    visualizer-chart-library doesn's exist, which might be the case for
    charts saved before the ChartJS charts were added.
    
    Fixees the issue reported in
    https://github.com/Codeinwp/visualizer/pull/531 and https://github.com/Codeinwp/visualizer/issues/506

--- classes/Visualizer/Gutenberg/Block.php
+++ classes/Visualizer/Gutenberg/Block.php
@@ -450,9 +450,19 @@ class Visualizer_Gutenberg_Block {
 	 */
 	public function add_rest_query_vars( $args, \WP_REST_Request $request ) {
 		if ( isset( $request['meta_key'] ) && isset( $request['meta_value'] ) ) {
-			$args['meta_key'] = $request->get_param( 'meta_key' );
-			$args['meta_value'] = $request->get_param( 'meta_value' );
-			$args['meta_compare'] = '!=';
+			$args['meta_query'] = array(
+				'relation'  => 'OR',
+				array(
+					'key'       => $request->get_param( 'meta_key' ),
+					'value'     => $request->get_param( 'meta_value' ),
+					'compare'   => '!=',
+				),
+				array(
+					'key'       => $request->get_param( 'meta_key' ),
+					'value'     => $request->get_param( 'meta_value' ),
+					'compare'   => 'NOT EXISTS',
+				),
+			);
 		}
 		return $args;
 	}
