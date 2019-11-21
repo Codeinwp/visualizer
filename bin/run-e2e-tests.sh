b6fe2bfbdec917adab43b63a7cd88f26dcaea0aa
Ashish Ravi <contactashish13@gmail.com>
2019-11-21 18:17:30 +0530

    make gutenberg as the first test

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -18,6 +18,12 @@ set -e
 
 export CYPRESS_HOST=$wp_host
 
+# gutenberg - DO NOT move this from here as the number of charts in the library are being counted.
+# so this HAS to be the first test.
+docker exec $args visualizer_wordpress wp --quiet plugin deactivate classic-editor
+export CYPRESS_SPEC_TO_RUN="free-gutenberg.js"
+npm run cypress:run
+
 # test free - lifecycle
 export CYPRESS_SPEC_TO_RUN="free-lifecycle.js"
 npm run cypress:run
@@ -26,7 +32,3 @@ npm run cypress:run
 export CYPRESS_SPEC_TO_RUN="free-sources.js"
 npm run cypress:run
 
-# gutenberg
-docker exec $args visualizer_wordpress wp --quiet plugin deactivate classic-editor
-export CYPRESS_SPEC_TO_RUN="free-gutenberg.js"
-npm run cypress:run
