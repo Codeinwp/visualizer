718d97483e9c6a349c045c321de927d9ed5f3961
Ashish Ravi <contactashish13@gmail.com>
2019-11-28 12:01:10 +0530

    remove irrelevant code

--- cypress/integration/free-gutenberg.js
+++ cypress/integration/free-gutenberg.js
@@ -11,79 +11,9 @@ describe('Test Free - gutenberg', function() {
 
     it('Create all charts', function() {
         cy.create_available_charts(Cypress.env('chart_types').free);
-//cy.create_available_charts(1);
     });
 
-    it.skip('Verify insertion of charts - using load more', function() {
-        cy.visit('/post-new.php');
-
-        // get rid of that irritating popup
-        cy.get('.nux-dot-tip__disable').click();
-
-        // insert a visualizer block
-        cy.get('div.edit-post-header-toolbar .block-editor-inserter button').click();
-        cy.get('.components-popover__content').then(function ($popup) {
-            cy.wrap($popup).find('.block-editor-inserter__search').type('visua');
-            cy.wrap($popup).find('.block-editor-inserter__results ul.block-editor-block-types-list li').should('have.length', 1);
-            cy.wrap($popup).find('.block-editor-inserter__results ul.block-editor-block-types-list li button').click();
-        });
-
-        // see the block has the correct elements.
-        cy.get('div[data-type="visualizer/chart"]').should('have.length', 1);
-        cy.get('.visualizer-settings__content-option').should('have.length', 2);
-
-        cy.get('.visualizer-settings__content-option').last().click();
-        cy.wait( Cypress.env('wait') );
-
-        // add all charts one by one - insert, go back, insert, go back...
-        var $pages = [];
-        for(var i = 0; i < Math.ceil(parseInt(Cypress.env('chart_types').free)/6); i++){
-            $pages.push(i);
-        }
-$pages = [0];
-        cy.wrap($pages).each((page, i, array) => {
-            var charts = [];
-            cy.wrap(page).then( () => {
-                for(var i = page * 6; i < ( ( (page * 6) + 6 ) > Cypress.env('chart_types').free ? Cypress.env('chart_types').free : ( (page * 6) + 6 ) ); i++){
-                    charts.push(i + 1);
-                }
-charts = [1];
-            }).then( () => {
-                cy.wrap(charts).each((num, i, array) => {
-                    if(page > 0){
-                        // click load more for every page except the first.
-                        cy.get('.visualizer-settings').then( ($div) => {
-                            cy.wrap($div).find('.visualizer-settings__charts .components-button').click();
-                        });
-                        cy.wait( Cypress.env('wait') );
-                    }
-
-                    cy.get('.visualizer-settings').then( ($div) => {
-                        // insert the chart.
-                        cy.wrap($div).find('.visualizer-settings__charts-single:nth-child(' + num + ')').then( ($chart) => {
-                            cy.wrap($chart).find('.visualizer-settings__charts-controls').click();
-                        });
-                    })
-                    .then( () => {
-                        cy.wait( Cypress.env('wait') );
-                        cy.get('.visualizer-settings').then( ($div) => {
-                            // check if inserted
-                            cy.wrap($div).find('.visualizer-settings__chart').should('have.length', 1);
-                            cy.wrap($div).find('.visualizer-settings__chart > div').should('have.length', 1);
-                            cy.wrap($div).find('.components-button-group button').should('have.length', 2);
-
-                            // go back to insert another one.
-                            cy.wrap($div).find('.components-button-group button').first().click();
-                            cy.wait( Cypress.env('wait') );
-                        });
-                    });
-                });
-            });
-        });
-    });
-
-
-    it('Verify insertion of charts - not using load more', function() {
+    it('Verify insertion of charts', function() {
         cy.visit('/post-new.php');
 
         // get rid of that irritating popup
@@ -109,7 +39,6 @@ charts = [1];
         for(var i = 1; i <= parseInt(Cypress.env('chart_types').free); i++){
             charts.push(i);
         }
-        //charts.push(1);
 
         cy.wrap(charts).each((num, i, array) => {
             cy.get('.visualizer-settings .visualizer-settings__charts-single:nth-child(' + num + ')').then( ($chart) => {
