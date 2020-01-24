26ff6d5b0c39c400eb45abd16fa33c6c6c683dc5
Ashish Ravi <contactashish13@gmail.com>
2020-01-24 11:03:50 +0530

    remove comments

--- bin/run-e2e-tests-gutenberg.sh
+++ bin/run-e2e-tests-gutenberg.sh
@@ -18,8 +18,6 @@ set -e
 
 export CYPRESS_HOST=$wp_host
 
-# gutenberg - DO NOT move this from here as the number of charts in the library are being counted.
-# so this HAS to be the first test.
 docker exec $args visualizer_wordpress wp --quiet plugin deactivate classic-editor
 export CYPRESS_SPEC_TO_RUN="free-gutenberg.js"
 npm run cypress:run
