b77c019b33857a6e981b4e88dab9fcc93ff878ca
Ashish Ravi <contactashish13@gmail.com>
2020-02-05 11:44:51 +0530

    grunt

--- classes/Visualizer/Render/Library.php
+++ classes/Visualizer/Render/Library.php
@@ -149,7 +149,7 @@ class Visualizer_Render_Library extends Visualizer_Render {
 		$sources = array_filter( $sources );
 		uasort(
 			$sources, function( $a, $b ) {
-				if ( $a == $b ) {
+				if ( $a === $b ) {
 					return 0;
 				}
 				return ( $a < $b ) ? -1 : 1;
