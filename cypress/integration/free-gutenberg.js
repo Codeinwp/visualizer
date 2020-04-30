3462265b0fe5a8dcaec7c90d16e2cb72d0eeea0a
Ashish Ravi <contactashish13@gmail.com>
2020-04-30 15:45:22 +0530

    build: disable welcome guide

--- cypress/integration/free-gutenberg.js
+++ cypress/integration/free-gutenberg.js
@@ -9,46 +9,14 @@ describe('Test Free - gutenberg', function() {
         cy.get('#wp-submit').click();
     });
 
-    it('temp test', function() {
-        cy.create_available_charts(1);
-        cy.visit('/post-new.php');
-        cy.clear_welcome();
-
-        var charts = [];
-        for(var i = 1; i <= 1; i++){
-            charts.push(i);
-        }
-
-        cy.wrap(charts).each((value, i, array) => {
-            // insert a visualizer block
-            cy.get('div.edit-post-header-toolbar .block-editor-inserter button').click();
-            cy.get('.components-popover__content').then(function ($popup) {
-                cy.wrap($popup).find('.block-editor-inserter__search').type('visua');
-                cy.wrap($popup).find('.block-editor-inserter__results ul.block-editor-block-types-list li').should('have.length', 1);
-                cy.wrap($popup).find('.block-editor-inserter__results ul.block-editor-block-types-list li button').click();
-            });
-
-            // see the block has the correct elements.
-            cy.get('div[data-type="visualizer/chart"]').should('have.length', (i + 2));
-
-            cy.get('div[data-type="visualizer/chart"]:nth-child(' + (i + 1) + ')').then( ($block) => {
-                cy.wrap($block).find('.visualizer-settings__content-option').should('have.length', 2);
-                cy.wrap($block).find('.visualizer-settings__content-option').last().click({force:true});
-
-                cy.wrap($block).find('.visualizer-settings .visualizer-settings__charts-single:nth-child(' + (i + 1) + ') .visualizer-settings__charts-controls').click();
-                cy.wrap($block).find('.visualizer-settings .visualizer-settings__chart').should('have.length', 1);
-                cy.wrap($block).find('.visualizer-settings .visualizer-settings__chart > div').should('have.length', 2);
-                cy.wrap($block).find('.visualizer-settings .components-button-group button').should('have.length', 2);
-            });
-        });
-
+    it.skip('temp test', function() {
     });
 
-    it.skip('Create all charts', function() {
+    it('Create all charts', function() {
         cy.create_available_charts(Cypress.env('chart_types').free);
     });
 
-    it.skip('Verify insertion of charts', function() {
+    it('Verify insertion of charts', function() {
         cy.visit('/post-new.php');
 
         cy.clear_welcome();
@@ -68,7 +36,7 @@ describe('Test Free - gutenberg', function() {
             });
 
             // see the block has the correct elements.
-            cy.get('div[data-type="visualizer/chart"]').should('have.length', (i + 1));
+            cy.get('div[data-type="visualizer/chart"]').should('have.length', (i + 2));
 
             cy.get('div[data-type="visualizer/chart"]:nth-child(' + (i + 1) + ')').then( ($block) => {
                 cy.wrap($block).find('.visualizer-settings__content-option').should('have.length', 2);
