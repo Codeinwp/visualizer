34afedff7278aa1eab7ebdc08c8a014a2f845913
Ashish Ravi <contactashish13@gmail.com>
2020-04-30 12:45:25 +0530

    build: disable welcome guide

--- cypress/integration/free-gutenberg.js
+++ cypress/integration/free-gutenberg.js
@@ -9,16 +9,16 @@ describe('Test Free - gutenberg', function() {
         cy.get('#wp-submit').click();
     });
 
-    it('temp test', function() {
+    it.skip('temp test', function() {
         cy.visit('/post-new.php');
         cy.clear_welcome();
     });
 
-    it.skip('Create all charts', function() {
+    it('Create all charts', function() {
         cy.create_available_charts(Cypress.env('chart_types').free);
     });
 
-    it.skip('Verify insertion of charts', function() {
+    it('Verify insertion of charts', function() {
         cy.visit('/post-new.php');
 
         cy.clear_welcome();
