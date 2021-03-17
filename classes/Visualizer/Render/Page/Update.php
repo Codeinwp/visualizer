a033e8950fa2de48b76d6c8070f18116646142f6
girishkrishaweb <girish@krishaweb.com>
2021-03-17 18:53:36 +0530

    Permissions tab disappears after modify chart #728

--- classes/Visualizer/Render/Page/Update.php
+++ classes/Visualizer/Render/Page/Update.php
@@ -56,7 +56,7 @@ class Visualizer_Render_Page_Update extends Visualizer_Render_Page {
 			}
 			echo 'win.vizUpdateChartPreview();';
 
-			echo $this->updateEditorAndSettings();
+			// echo $this->updateEditorAndSettings();
 
 			echo '}';
 
