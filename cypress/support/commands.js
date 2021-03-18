5d09f2c41c51ab6664e37561001445233d8ac88f
girishkrishaweb <girish@krishaweb.com>
2021-03-18 15:25:26 +0530

    Fix CyPress issue and remove cy wait function.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -176,7 +176,6 @@ Cypress.Commands.add( 'test_advanced_settings', ($create_new_chart) => {
     }
 
     cy.visit(Cypress.env('urls').library ).then(() => {
-        cy.wait( Cypress.env('wait') );
         const id = Cypress.$('div.visualizer-chart div.visualizer-chart-canvas').first().attr('id');
         first_chart = Cypress.$('#' + id).html();
     });
@@ -222,7 +221,6 @@ Cypress.Commands.add( 'test_advanced_settings', ($create_new_chart) => {
     });
 
     cy.visit(Cypress.env('urls').library ).then(() => {
-        cy.wait( Cypress.env('wait') );
         const id = Cypress.$('div.visualizer-chart div.visualizer-chart-canvas').first().attr('id');
         var content = Cypress.$('#' + id).html();
         expect(content).to.not.equal(first_chart);
