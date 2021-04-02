a754f05b2b7002d707ec67d67a7184d92d0d8f42
girishkrishaweb <girish@krishaweb.com>
2021-04-02 18:13:45 +0530

    Remove series type row export CSV/Excel #772

--- classes/Visualizer/Module.php
+++ classes/Visualizer/Module.php
@@ -266,6 +266,8 @@ class Visualizer_Module {
 
 		$bom = chr( 0xEF ) . chr( 0xBB ) . chr( 0xBF );
 		$fp = tmpfile();
+		unset( $rows[1] );
+		$rows = array_values( $rows );
 		// support for MS Excel
 		fprintf( $fp, $bom );
 		foreach ( $rows as $row ) {
@@ -312,12 +314,18 @@ class Visualizer_Module {
 		// PHPExcel did not like sheet names longer than 31 characters and we will assume the same with PhpSpreadsheet
 		$chart      = substr( $filename, 0, 30 );
 		$filename   .= '.xlsx';
-
+		unset( $rows[1] );
+		$rows = array_values( $rows );
+		$rows = array_map(
+			function( $r ) {
+				return array_map( 'strval', $r );
+			},
+			$rows
+		);
 		$vendor_file = VISUALIZER_ABSPATH . '/vendor/autoload.php';
 		if ( is_readable( $vendor_file ) ) {
 			include_once( $vendor_file );
 		}
-
 		$xlsData    = '';
 		if ( class_exists( 'PhpOffice\PhpSpreadsheet\Spreadsheet' ) ) {
 			$doc        = new PhpOffice\PhpSpreadsheet\Spreadsheet();
