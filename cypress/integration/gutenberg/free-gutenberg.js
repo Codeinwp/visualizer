ba6960ec602f187318b92b79a7a6ae8c9676523f
girishkrishaweb <girish@krishaweb.com>
2021-07-26 18:27:44 +0530

    Fix cypress test issue in WordPress 5.8 #827

--- cypress/integration/gutenberg/free-gutenberg.js
+++ cypress/integration/gutenberg/free-gutenberg.js
@@ -27,7 +27,7 @@ describe('Test Free - gutenberg', function() {
         cy.wrap(charts).each((value, i, array) => {
             // insert a visualizer block
             cy.get('div.edit-post-header__toolbar button.edit-post-header-toolbar__inserter-toggle').click();
-            cy.get('.edit-post-layout__inserter-panel-content').then(function ($popup) {
+            cy.get('.edit-post-editor__inserter-panel-content').then(function ($popup) {
                 cy.wrap($popup).find('.block-editor-inserter__search-input').type('visua');
                 cy.wrap($popup).find('.block-editor-block-types-list .editor-block-list-item-visualizer-chart').should('have.length', 1);
                 cy.wrap($popup).find('.block-editor-block-types-list .editor-block-list-item-visualizer-chart').click();
