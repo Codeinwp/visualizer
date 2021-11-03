2f4d1af3e77147fb7403dd22affc5984acb50b1b
girishkrishaweb <girish@krishaweb.com>
2021-11-03 15:59:43 +0530

    Improve script loader #851

--- classes/Visualizer/Module/Frontend.php
+++ classes/Visualizer/Module/Frontend.php
@@ -399,22 +399,29 @@ class Visualizer_Module_Frontend extends Visualizer_Module {
 
 		$actions_div            .= $css;
 
+		$_charts = array();
+		$_charts_type = '';
 		foreach ( $this->_charts as $id => $array ) {
+			$_charts = $this->_charts;
 			$library = $array['library'];
-			wp_register_script(
-				"visualizer-render-$library",
-				VISUALIZER_ABSURL . 'js/render-facade.js',
-				apply_filters( 'visualizer_assets_render', array( 'jquery', 'visualizer-customization' ), true ),
-				Visualizer_Plugin::VERSION,
-				true
-			);
-
-			wp_enqueue_script( "visualizer-render-$library" );
+			$_charts_type = $library;
+			if ( ! wp_script_is( "visualizer-render-$library", 'registered' ) ) {
+				wp_register_script(
+					"visualizer-render-$library",
+					VISUALIZER_ABSURL . 'js/render-facade.js',
+					apply_filters( 'visualizer_assets_render', array( 'jquery', 'visualizer-customization' ), true ),
+					Visualizer_Plugin::VERSION,
+					true
+				);
+				wp_enqueue_script( "visualizer-render-$library" );
+			}
+		}
+		if ( wp_script_is( "visualizer-render-$_charts_type" ) ) {
 			wp_localize_script(
-				"visualizer-render-$library",
+				"visualizer-render-$_charts_type",
 				'visualizer',
 				array(
-					'charts'        => $this->_charts,
+					'charts'        => $_charts,
 					'language'      => $this->get_language(),
 					'map_api_key'   => get_option( 'visualizer-map-api-key' ),
 					'rest_url'      => version_compare( $wp_version, '4.7.0', '>=' ) ? rest_url( 'visualizer/v' . VISUALIZER_REST_VERSION . '/action/#id#/#type#/' ) : '',
@@ -426,8 +433,8 @@ class Visualizer_Module_Frontend extends Visualizer_Module {
 					'is_front'  => true,
 				)
 			);
-			wp_enqueue_style( 'visualizer-front' );
 		}
+		wp_enqueue_style( 'visualizer-front' );
 
 		// return placeholder div
 		return $actions_div . '<div id="' . $id . '"' . $this->getHtmlAttributes( $attributes ) . '></div>' . $this->addSchema( $chart->ID );
