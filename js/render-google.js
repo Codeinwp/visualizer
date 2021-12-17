7098eb27d5971c7896efa825a34c19f4c27181d2
girishkrishaweb <girish@krishaweb.com>
2021-12-17 15:31:44 +0530

    Fix multiple chart render issue Codeinwp/visualizer-pro#324

--- js/render-google.js
+++ js/render-google.js
@@ -434,6 +434,7 @@ var __visualizer_chart_images   = [];
     });
 
     $(window).on('load', function(){
+        $( '.visualizer-front:not(.visualizer-chart-loaded)' ).resize();
         resizeHiddenContainers(true);
     });
 
