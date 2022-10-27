77d4505794baeabfebe524cccac6e1b5a48f8187
girishpanchal30 <girish@krishaweb.com>
2022-10-27 16:59:33 +0530

    Fix float value issue in dataTable chart #955

--- js/render-datatables.js
+++ js/render-datatables.js
@@ -247,6 +247,9 @@
                 }
                 if(typeof series.format.decimal !== ''){
                     parts[1] = series.format.decimal;
+                    if ( '' === series.format.precision ) {
+                        series.format.precision = 2;
+                    }
                 }
                 if(typeof series.format.precision !== '' && parseInt(series.format.precision) > 0){
                     parts[2] = series.format.precision;
