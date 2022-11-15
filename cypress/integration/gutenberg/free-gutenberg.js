60c1b897a56c8844b6583d3a3e5bd2a04206c66a
girishpanchal30 <girish@krishaweb.com>
2022-11-15 21:04:21 +0530

    e2e: fix invisible element error #960

--- cypress/integration/gutenberg/free-gutenberg.js
+++ cypress/integration/gutenberg/free-gutenberg.js
@@ -18,7 +18,8 @@ describe('Test Free - gutenberg', function() {
     it('Verify insertion of charts', function() {
         cy.visit('/wp-admin/post-new.php');
 
-        cy.clear_welcome();
+        // get rid of that irritating popup
+        cy.get('.edit-post-welcome-guide .components-modal__header button').click();
 
         var charts = Array.from({ length: parseInt(Cypress.env('chart_types').free - 1) }, function(_item, index) {
             return index + 1;
