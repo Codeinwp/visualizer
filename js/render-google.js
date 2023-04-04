270ba75560ec0a6c432dde0680c1de0fed8b96a3
girishpanchal30 <girish@krishaweb.com>
2023-04-04 14:39:27 +0530

    Fix empty chart issue #997

--- js/render-google.js
+++ js/render-google.js
@@ -571,7 +571,7 @@ var chartWrapperError = [];
         objects = {};
         if ( 'object' === typeof google ) {
             $chart_types.push( 'controls' );
-            google.load( 'visualization', 'current', {packages: $chart_types, mapsApiKey: v.map_api_key, 'language' : v.language,
+            google.load( 'visualization', '46', {packages: $chart_types, mapsApiKey: v.map_api_key, 'language' : v.language,
                 callback: function () {
                     gv = google.visualization;
                     all_charts = v.charts;
