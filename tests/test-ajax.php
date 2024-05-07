265fc5a7dd21c2bd58507bfb3e842de48bfcda05
Bogdan Preda <bogdan.preda@themeisle.com>
2024-05-07 17:54:08 +0300

    chore: code style

--- tests/test-ajax.php
+++ tests/test-ajax.php
@@ -253,13 +253,13 @@ class Test_Visualizer_Ajax extends WP_Ajax_UnitTestCase {
 	 * Test the query stripping of comments.
 	 */
 	public function test_sql_comment_strip() {
-		$source = new Visualizer_Source_Query("SELECT * FROM test_table /* WHERE post_type = 'post' */");
-		$this->assertEquals( "SELECT * FROM test_table", $source->get_query() );
+		$source = new Visualizer_Source_Query( "SELECT * FROM test_table /* WHERE post_type = 'post' */");
+		$this->assertEquals( 'SELECT * FROM test_table', $source->get_query() );
 
-		$source = new Visualizer_Source_Query("SELECT * FROM test_table -- WHERE post_type = 'post'");
-		$this->assertEquals( "SELECT * FROM test_table", $source->get_query() );
+		$source = new Visualizer_Source_Query( "SELECT * FROM test_table -- WHERE post_type = 'post'");
+		$this->assertEquals( 'SELECT * FROM test_table', $source->get_query() );
 
-		$source = new Visualizer_Source_Query("/* SELECT */ DELETE * FROM test_table /* WHERE post_type = 'post' */");
-		$this->assertEquals( "DELETE * FROM test_table", $source->get_query() );
+		$source = new Visualizer_Source_Query( "/* SELECT */ DELETE * FROM test_table /* WHERE post_type = 'post' */");
+		$this->assertEquals( 'DELETE * FROM test_table', $source->get_query() );
 	}
 }
