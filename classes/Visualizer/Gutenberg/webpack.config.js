88e1635d559ffa04b264619e06e9abc6b0d96ada
girishpanchal30 <girish@krishaweb.com>
2024-11-04 15:04:24 +0530

    chore: update npm packages to latest versions

--- classes/Visualizer/Gutenberg/webpack.config.js
+++ classes/Visualizer/Gutenberg/webpack.config.js
@@ -1,93 +1,108 @@
-const webpack = require( 'webpack' );
+const webpack = require('webpack');
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-	externals: {
+    externals: {
         'lodash': 'lodash'
     },
-	mode: NODE_ENV,
-	entry: './src/index.js',
-	output: {
-		path: __dirname,
-		filename: './build/block.js',
-		chunkFilename: './build/[name].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /.js?$/,
-				use: [ {
-					loader: 'babel-loader',
-					options: {
-						presets: [ '@babel/preset-env' ],
-						plugins: [
-							'@babel/plugin-transform-async-to-generator',
-							'@babel/plugin-proposal-object-rest-spread',
-							[
-								'@babel/plugin-transform-react-jsx', {
-									'pragma': 'wp.element.createElement'
-								}
-							]
-						]
-					}
-				},
-				'eslint-loader' ],
-				exclude: /node_modules/
-			},
-			{
-				test: /\.(css|scss)$/,
-				use: [ {
-					loader: MiniCssExtractPlugin.loader
-				},
-				'css-loader',
-				{
-					loader: 'postcss-loader',
-					options: {
-						plugins: [
-							require( 'autoprefixer' )
-						]
-					}
-				},
-				{
-					loader: 'sass-loader',
-					query: {
-						outputStyle:
-							'production' === process.env.NODE_ENV ? 'compressed' : 'nested'
-					}
-				} ]
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: './[name].[ext]'
-						}
-					}
-				]
-			}
-		]
-	},
-	optimization: {
+    mode: NODE_ENV,
+    entry: {
+    	block: './src/index.js'
+    },
+    output: {
+        path: __dirname,
+		filename: './build/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            '@babel/plugin-transform-async-to-generator',
+                            '@babel/plugin-proposal-object-rest-spread',
+                            [
+                                '@babel/plugin-transform-react-jsx', {
+                                    'pragma': 'wp.element.createElement'
+                                }
+                            ]
+                        ]
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                outputStyle: NODE_ENV === 'production' ? 'compressed' : 'expanded'
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: './[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
 		splitChunks: {
 			cacheGroups: {
 				handsontable: {
 					name: 'handsontable',
 					test: /[\\/]node_modules[\\/]handsontable/,
-					chunks: 'all'
+					chunks: 'all',
+					enforce: true
 				}
 			}
 		}
 	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify( NODE_ENV )
-		}),
-		new MiniCssExtractPlugin({
-			filename: './build/block.css',
-			chunkFilename: './build/[name].css'
-		})
-	]
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+        }),
+        new MiniCssExtractPlugin({
+            filename: './build/[name].css'
+        }),
+        new TerserPlugin({
+            terserOptions: {
+                format: {
+                    comments: false  // Disable comments
+                }
+            },
+            extractComments: false  // Prevents LICENSE.txt generation
+        })
+    ]
 };
