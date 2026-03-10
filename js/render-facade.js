184ae3e499428b27d05d09f648d7bc77cd453044
Hardeep Asrani <hardeepasrani@gmail.com>
2026-03-10 23:24:12 +0530

    fix: prevent double clipboard alert on copy action (#1265)
    * fix: prevent double clipboard alert on copy action
    
    render-facade.js could be loaded more than once (registered under
    multiple handles when charts from different libraries appear on the
    same page). The copy ClipboardJS instance had no guard against
    double-initialization, causing the success alert to fire twice.
    
    Added vizClipboard2 guard to mirror the existing vizClipboard1
    pattern already used for the shortcode copy button.
    
    * fix: copy still happening twice
    
    * chore: bump version for dev builds
    
    * chore: bump version for dev builds
    
    * chore: bump version for dev builds
    
    * chore: bump version for dev builds

--- js/render-facade.js
+++ js/render-facade.js
@@ -1,7 +1,8 @@
 /* global console */
 /* global visualizer */
 /* global jQuery */
-var vizClipboard1=null;
+window['vizClipboard1'] = window['vizClipboard1'] || null;
+window['vizClipboard2'] = window['vizClipboard2'] || null;
 (function($, visualizer){
     
     function initActionsButtons(v) {
@@ -13,12 +14,12 @@ var vizClipboard1=null;
             });
         }
 
-        if($('a.visualizer-action[data-visualizer-type=copy]').length > 0) {
+        if($('a.visualizer-action[data-visualizer-type=copy]').length > 0 && vizClipboard2 === null) {
             $('a.visualizer-action[data-visualizer-type=copy]').on('click', function(e) {
                 e.preventDefault();
             });
-            var clipboard = new ClipboardJS('a.visualizer-action[data-visualizer-type=copy]'); // jshint ignore:line
-            clipboard.on('success', function(e) {
+            vizClipboard2 = new ClipboardJS('a.visualizer-action[data-visualizer-type=copy]'); // jshint ignore:line
+            vizClipboard2.on('success', function(e) {
                 window.alert(v.i10n['copied']);
             });
         }
