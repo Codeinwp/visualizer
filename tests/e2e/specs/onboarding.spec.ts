f42382faca107ab69ce1f13a257ac43cf7d49657
Hardeep Asrani <hardeepasrani@gmail.com>
2026-03-30 16:21:48 +0530

    feat: improvements to onboarding flow (#1295)
    * feat: improvements to onboarding flow
    
    * fix: onboarding not redrecting
    
    * fix: spc stopping wizard flow

--- tests/e2e/specs/onboarding.spec.ts
+++ tests/e2e/specs/onboarding.spec.ts
@@ -31,56 +31,44 @@ test.describe( 'Onboarding', () => {
 
     test( 'Check the setup wizard', async ( { page, admin} ) => {
         await admin.visitAdminPage( 'admin.php?page=visualizer-setup-wizard#step-1' );
+        await page.addStyleTag({ content: '#wpfooter{display:none !important;}' });
 
         // Step 1
         await expect(page.getByLabel('Pie/Donut chart', { exact: true })).toBeVisible();
         await page.getByLabel('Bar chart', { exact: true }).check();
         await expect(page.getByLabel('Line chart', { exact: true })).toBeVisible();
         await expect(page.getByLabel('Bar chart', { exact: true })).toBeVisible();
-        await page.getByLabel('Next', { exact: true }).click();
-        await page.getByLabel('Next', { exact: true }).click();
-        await page.getByLabel('Next', { exact: true }).click();
-        await page.getByLabel('Next', { exact: true }).click();
-        await expect(page.getByText('Discover the power of PRO! 11')).toBeVisible();
-        await expect(page.getByRole('button', { name: 'Save And Continue ' })).toBeVisible();
+        await expect(page.getByRole('button', { name: /Save And Continue/i })).toBeVisible();
         await expect(page.getByRole('link', { name: ' Go to dashboard' })).toBeVisible();
-        await page.getByRole('button', { name: 'Save And Continue ' }).click();
+        await page.getByRole('button', { name: /Save And Continue/i }).click();
 
         // Step 2
-        await expect(page.getByLabel('You\'re almost done! We use').locator('h2')).toContainText('You\'re almost done!');
-        await expect(page.getByRole('heading', { name: 'Importing demo data' })).toBeVisible();
-        await expect(page.getByText('Done! Demo data has been successfully imported.')).toBeVisible();
-        await expect(page.getByLabel('You\'re almost done! We use')).toContainText('Import data from other charts, WordPress, databases, or manual data entries using Visualizer');
-        await page.getByRole('button', { name: 'Continue ' }).click();
+        await expect(page.getByRole('heading', { name: 'Insert a chart into the draft page' })).toBeVisible();
+        await expect(page.getByRole('button', { name: /Save And Continue/i })).toBeVisible();
+        await page.getByRole('button', { name: /Save And Continue/i }).click();
 
         // Step 3
-        await expect(page.getByRole('heading', { name: 'Insert a chart into the draft page' })).toBeVisible();
-        await expect(page.getByRole('button', { name: 'Create a draft page We will' })).toBeVisible();
-        await page.getByRole('button', { name: 'Create a draft page We will' }).click();
-        await expect(page.getByRole('heading', { name: 'Chart preview' })).toBeVisible({ timeout: 15000 });
-        await page.getByRole('button', { name: 'Create a draft page We will' }).click();
-        await page.getByRole('button', { name: 'Save And Continue ' }).click();
+        await expect(page.getByRole('heading', { name: 'You\'re all set!' })).toBeVisible();
+        await expect(page.getByText('Boost your website speed')).toBeVisible();
+        await expect(page.getByText('Build better pages')).toBeVisible();
+        await expect(page.getByText('Cache your pages')).toBeVisible();
+        await expect(page.getByText('Stay in the loop')).toBeVisible();
 
-        // Step 4
-        await expect(page.getByRole('heading', { name: 'Extra Features' })).toBeVisible();
-        await expect(page.getByRole('button', { name: 'Enable perfomance features' })).toBeVisible();
-        await page.getByRole('button', { name: 'Enable perfomance features' }).click();
-        await expect(page.getByLabel('Extra Features We\'re').getByRole('listitem')).toContainText('Improve your website speed and images by 80% with Optimole');
-        await expect(page.getByRole('button', { name: 'Improve now' })).toBeVisible();
-        await page.getByRole('checkbox').uncheck();
-        await expect(page.getByLabel('Extra Features We\'re')).toContainText('Skip Improvement');
-        await page.getByRole('button', { name: 'Skip Improvement' }).click();
+        const optimoleCheckbox = page.locator('#enable_performance');
+        if (await optimoleCheckbox.isEnabled()) {
+            await optimoleCheckbox.uncheck();
+        }
+        const otterCheckbox = page.locator('#enable_otter_blocks');
+        if (await otterCheckbox.isEnabled()) {
+            await otterCheckbox.uncheck();
+        }
+        const spcCheckbox = page.locator('#enable_page_cache');
+        if (await spcCheckbox.isEnabled()) {
+            await spcCheckbox.uncheck();
+        }
+        await page.locator('#enable_newsletter').uncheck();
 
-        // Step 5
-        await expect(page.getByRole('heading', { name: 'Updates, tutorials, special offers, and more' })).toBeVisible();
-        await expect(page.getByLabel('Updates, tutorials, special')).toContainText('Get exclusive access to the Visualizer newsletter.');
-        await expect(page.getByLabel('Updates, tutorials, special').locator('img')).toBeVisible();
-        await expect(page.getByPlaceholder('test1@xx.com')).toBeVisible();
-        await page.getByPlaceholder('test1@xx.com').click();
-        await page.getByPlaceholder('test1@xx.com').fill('test@email.com');
-        await expect(page.getByLabel('Updates, tutorials, special')).toContainText('Send Me Access');
-        await expect(page.getByLabel('Updates, tutorials, special')).toContainText('Skip, Don’t give me access');
-        await page.getByRole('button', { name: 'Skip, Don’t give me access' }).click();
+        await expect(page.getByRole('button', { name: 'Finish Setup' })).toBeVisible();
     } );
 
 } );
